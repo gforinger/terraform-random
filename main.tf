4c73982fa35f3416f78bf2bd62961c4816b6d57d
Greg Foringer <Greg.Foringer@nccgroup.trust>
2019-07-09 11:14:43 -0700

    payload4

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "random_id" "random" {
 }
 
 output "payload" {
-  value = "javascript:/*--></title></style></textarea></script></xmp><svg/onload='+/"/+/onmouseover=1/+/[*/[]/+alert(1)//'>"
+  value = "none"
 }
 
 output "random" {
